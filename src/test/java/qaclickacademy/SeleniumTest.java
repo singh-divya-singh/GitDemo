91ff5657152b207e26e74cd8745e3254c9779c60
divya <singhdivya34567@gmail.com>
2021-06-05 08:43:32 +0530

    American guy commit

--- src/test/java/qaclickacademy/SeleniumTest.java
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -9,7 +9,8 @@ public class SeleniumTest {
 	@Test
 	public void BrowserAUtomation()
 	{
-		System.out.println("BrowserAUtomation");	
+		System.out.println("BrowserAUtomation");
+		System.out.println("Web AUtomation");
 	}
 	@Test
 	public void elementsUi()
